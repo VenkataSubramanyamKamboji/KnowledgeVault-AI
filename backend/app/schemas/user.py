eeb5ea48eec578d2257862d0b60baff0afec4d45
VenkataSubramanyamKamboji <subramanyamkamboji@gmail.com>
2026-07-14 11:59:18 +0530

    Start current user authentication

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -10,3 +10,12 @@ class UserCreate(BaseModel):
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
+
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+
+    model_config = {
+        "from_attributes": True
+    }
